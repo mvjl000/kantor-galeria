7ab5edffdb479f4d3b9fed4d76662eec98a4af9e
mvjl000 <milosz.devmail@gmail.com>
2022-04-03 14:54:08 +0200

    Implement theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from 'next';
+import styled from '@emotion/styled';
+
+const H1 = styled.h1`
+  color: ${({ theme }) => theme.fontColors.grey};
+  font-size: ${({ theme }) => theme.fontSizes.large};
+`;
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Hello nextjs</h1>
+      <H1>Hello nextjs</H1>
     </div>
   );
 };
---
+++ styles/theme.ts
@@ -0,0 +1,21 @@
+export const theme = {
+  colors: {
+    white: 'hsl(0, 0%, 100%)',
+    black: 'hsl(0, 0%, 0%)',
+    grey: 'hsl(0, 0%, 80%)',
+  },
+  fontColors: {
+    black: 'hsl(0, 0%, 0%)',
+    grey: 'hsl(0, 0%, 40%)',
+  },
+  fontSizes: {
+    xSmall: '1.5rem',
+    small: '1.8rem',
+    medium: '2.4rem',
+    large: '3.4rem',
+  },
+  zIndex: {
+    modal: 10,
+    menu: 100,
+  },
+};
