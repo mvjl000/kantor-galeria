162b03d744637e81af9e890e62d77f5f23c1c2d0
mvjl000 <milosz.devmail@gmail.com>
2022-04-05 19:22:52 +0200

    Create css mixins

---
+++ styles/mixins.ts
@@ -0,0 +1,27 @@
+import { css } from '@emotion/react';
+
+export const flexBetween = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const flexCenter = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const flexColumnBetween = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const flexColumnCenter = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
