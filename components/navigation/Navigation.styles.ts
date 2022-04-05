0ffa4971cbe458940b74ee47992dd853d30f589d
mvjl000 <milosz.devmail@gmail.com>
2022-04-05 19:49:00 +0200

    Create burger button

--- components/navigation/Navigation.styles.ts
+++ components/navigation/Navigation.styles.ts
@@ -3,8 +3,42 @@ import { flexBetween } from '../../styles/mixins';
 
 export const Wrapper = styled.nav`
   ${flexBetween};
-  margin: 20px;
+  margin: 25px;
   img {
     width: 200px;
+  }
+`;
+
+export const LogoWrapper = styled.div`
+  width: 200px;
+  img {
+    width: 100%;
+  }
+
+  ${({ theme }) => theme.mq.desktop} {
+    width: 243px;
+  }
+`;
+
+export const BurgerButton = styled.button`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: 4rem;
+  height: 3.2rem;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  z-index: 10;
+  div {
+    width: 4rem;
+    height: 0.3rem;
+    background: ${({ theme }) => theme.colors.black};
+    position: relative;
+  }
+
+  ${({ theme }) => theme.mq.desktop} {
+    display: none;
   }
 `;
--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Wrapper } from './Navigation.styles';
+import { Wrapper, BurgerButton, LogoWrapper } from './Navigation.styles';
 
 const Navigation: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,12 +10,18 @@ const Navigation: React.FC = () => {
 
   return (
     <Wrapper>
-      <Link href="/" passHref={true}>
-        <a>
-          <Image src="/images/kantor_logo_black.png" alt="Logo kantoru" width={243} height={64} />
-        </a>
-      </Link>
-      <button>Burger</button>
+      <LogoWrapper>
+        <Link href="/" passHref={true}>
+          <a>
+            <Image src="/images/kantor_logo_black.png" alt="Logo kantoru" width={243} height={64} />
+          </a>
+        </Link>
+      </LogoWrapper>
+      <BurgerButton onClick={toggleSideMenu}>
+        <div />
+        <div />
+        <div />
+      </BurgerButton>
     </Wrapper>
   );
 };
