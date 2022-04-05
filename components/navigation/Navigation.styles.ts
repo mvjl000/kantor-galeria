41d69481af66f34ed69f2a347d737a168081500e
mvjl000 <milosz.devmail@gmail.com>
2022-04-05 19:23:19 +0200

    Create nav wrapper with logo

---
+++ components/navigation/Navigation.styles.ts
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import { flexBetween } from '../../styles/mixins';
+
+export const Wrapper = styled.nav`
+  ${flexBetween};
+  margin: 20px;
+  img {
+    width: 200px;
+  }
+`;
--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { Wrapper } from './Navigation.styles';
+
 const Navigation: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleSideMenu = () => setIsOpen(!isOpen);
 
   return (
-    <>
-      <h2>Navtigation</h2>
-    </>
+    <Wrapper>
+      <Link href="/" passHref={true}>
+        <a>
+          <Image src="/images/kantor_logo_black.png" alt="Logo kantoru" width={243} height={64} />
+        </a>
+      </Link>
+      <button>Burger</button>
+    </Wrapper>
   );
 };
 
