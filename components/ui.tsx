bef9fbf20dff881af3fe049139c9c4a66422a74f
Miłosz <milosz.devmail@gmail.com>
2022-10-12 12:55:17 +0200

    Add xLarge font size to theme

--- components/ui.tsx
+++ components/ui.tsx
@@ -4,11 +4,11 @@ export const H1 = styled.h1`
   margin-top: 40px;
   padding: 0 20px;
   color: ${({ theme }) => theme.font.color.black};
-  font-size: ${({ theme }) => theme.font.size.medium};
+  font-size: ${({ theme }) => theme.font.size.large};
   font-weight: ${({ theme }) => theme.font.weight.light};
   text-align: center;
 
   ${({ theme }) => theme.mq.desktop} {
-    font-size: ${({ theme }) => theme.font.size.large};
+    font-size: ${({ theme }) => theme.font.size.xLarge};
   }
 `;
--- emotion.d.ts
+++ emotion.d.ts
@@ -14,6 +14,7 @@ declare module '@emotion/react' {
         small: string;
         medium: string;
         large: string;
+        xLarge: string;
       };
       color: {
         black: string;
