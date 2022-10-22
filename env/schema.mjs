67911eab7ebba59c00844beb5b70ca479c329a53
Miłosz <milosz.devmail@gmail.com>
2022-10-22 19:10:07 +0200

    Add missing auth0 env vars

--- env/schema.mjs
+++ env/schema.mjs
@@ -3,9 +3,12 @@ import { z } from 'zod';
 export const serverSchema = z.object({
   API_URL: z.string().url(),
   UPLOADS_URL: z.string().url(),
-  AUTH0_DOMAIN: z.string(),
+  AUTH0_SECRET: z.string().length(64),
+  AUTH0_BASE_URL: z.string().url(),
+  AUTH0_ISSUER_BASE_URL: z.string().url(),
+  AUTH0_DOMAIN: z.string().url(),
   AUTH0_CLIENT_ID: z.string(),
   AUTH0_CLIENT_SECRET: z.string(),
-  AUTH0_ALLOWED_CALLBACK: z.string(),
-  AUTH0_ALLOWED_LOGOUT_URL: z.string(),
+  AUTH0_ALLOWED_CALLBACK: z.string().url(),
+  AUTH0_ALLOWED_LOGOUT_URL: z.string().url(),
 });
