a67c6b26c3dd2358752ed76bce382bb4347061b9
Miłosz <milosz.devmail@gmail.com>
2022-12-09 16:24:21 +0100

    Move useMemo hook up

--- components/AreaChart.tsx
+++ components/AreaChart.tsx
@@ -1,43 +1,43 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Legend, Line, LineChart, Tooltip, XAxis, YAxis, ResponsiveContainer } from 'recharts';
 import { PriceHistory } from '../pages/types';
 import { monthsFromatterPL } from '../utils/formatters';
 
 const data = [
   {
-    date: '31 MAR',
+    date: '2022-12-02T07:56:48.816Z',
     buy: 3.82,
     sell: 3.9,
   },
   {
-    date: '1 KWI',
+    date: '2022-12-03T07:56:48.816Z',
+    buy: 3.83,
+    sell: 3.91,
+  },
+  {
+    date: '2022-12-04T07:56:48.816Z',
+    buy: 3.79,
+    sell: 3.81,
+  },
+  {
+    date: '2022-12-05T07:56:48.816Z',
+    buy: 3.79,
+    sell: 3.81,
+  },
+  {
+    date: '2022-12-06T07:56:48.816Z',
+    buy: 3.85,
+    sell: 3.87,
+  },
+  {
+    date: '2022-12-07T07:56:48.816Z',
     buy: 3.86,
-    sell: 4,
+    sell: 3.88,
   },
   {
-    date: '2 KWI',
-    buy: 3.82,
-    sell: 3.9,
-  },
-  {
-    date: '3 KWI',
-    buy: 3.71,
-    sell: 3.74,
-  },
-  {
-    date: '4 KWI',
-    buy: 4.21,
-    sell: 4.32,
-  },
-  {
-    date: '5 KWI',
-    buy: 3.82,
-    sell: 3.9,
-  },
-  {
-    date: '6 KWI',
-    buy: 3.87,
-    sell: 3.92,
+    date: '2022-12-08T07:56:48.816Z',
+    buy: 3.75,
+    sell: 3.82,
   },
 ];
 
@@ -46,13 +46,24 @@ interface AreaChartProps {
 }
 
 const AreaChartComponent: React.FC<AreaChartProps> = ({ price_history }) => {
+  const yAxisRange = useMemo(() => {
+    const yAxisRangeValues = data.flatMap(({ buy, sell }) => {
+      return [buy, sell];
+    });
+
+    const minValue = Math.min(...yAxisRangeValues);
+    const maxValue = Math.max(...yAxisRangeValues);
+
+    return [minValue - 0.1, maxValue + 0.1];
+  }, []);
+
   if (!price_history) {
     return <p>Something went wrong</p>;
   }
 
   return (
     <ResponsiveContainer width="99%" height="99%">
-      <LineChart data={price_history} margin={{ top: 0, right: 50, left: 0, bottom: 0 }}>
+      <LineChart data={data} margin={{ top: 0, right: 50, left: 0, bottom: 0 }}>
         <XAxis
           dataKey="date"
           tickFormatter={(dateString) => {
@@ -61,7 +72,7 @@ const AreaChartComponent: React.FC<AreaChartProps> = ({ price_history }) => {
             return `${date.getDate()} ${monthsFromatterPL(date.getMonth())}`;
           }}
         />
-        <YAxis domain={[3.5, 4.5]} />
+        <YAxis domain={yAxisRange} />
         <Tooltip
           labelFormatter={(dateString) => {
             const date = new Date(dateString);
